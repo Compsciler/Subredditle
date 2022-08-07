30e1fa1dbcc6ae98acb235728eaa59afaf332892
Roger Wang <rogerwang2520@gmail.com>
2022-08-06 21:01:07 -0700

    Update shareable game link

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -8,7 +8,7 @@ const webShareApiDeviceTypes: string[] = ['mobile', 'smarttv', 'wearable']
 const parser = new UAParser()
 const browser = parser.getBrowser()
 const device = parser.getDevice()
-const gameUrl = 'rebrand.ly/subredditle'
+const gameUrl = 'subredditle.herokuapp.com'
 
 export const shareStatus = (
   solution: string,
